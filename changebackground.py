46cbef8df48f5c14288c93c3d8a5567fef39b7c5
unknown <z0039k7t@MD177F7C.ww002.siemens.net>
2014-01-23 14:25:40 +0000

    first commit

---
+++ changebackground.py
@@ -0,0 +1,21 @@
+import sys, ctypes, os, random
+
+# change background to new image (osx, doesn't work very well)
+def bgchg(p):
+    print "Changing wallpaper"
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER,0,p)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        if os.path.exists(sys.argv[1]):
+            files = [files for root, dirs, files in os.walk(sys.argv[1])]
+            files = files[0]
+            newbg = files[random.randint(0, len(files)-1)]
+            print "Changing background to: ", newbg
+            bgchg(os.path.join(sys.argv[1],newbg))
+        else:
+            print "Folder doesn't exist: ", sys.argv[1]
+    else:
+        print "Usage: {0} folder-of-backgrounds".format(sys.argv[0])
+        
